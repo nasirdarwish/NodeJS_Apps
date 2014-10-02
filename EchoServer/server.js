16b59c3a5057bd9421cdbdac7db953581c305082
nasirdarwish <darwish@kfupm.edu.sa>
2014-10-02 15:20:20 +0300

    Added FormExample

--- EchoServer/server.js
+++ EchoServer/server.js
@@ -1,12 +1,33 @@
 var http = require('http'); // require (use) the http module
 // create an HTTP server
-var myServer = http.createServer(function handleRequest(req, res) 
+
+
+
+
+function handleRequest(req, res) 
 {  // this function is called when a request is received
    res.writeHead(200, { 'Content-Type': 'text/plain' });
+   var bodyData='';
+
+   req.on("data", function (err,data) 
+   { bodyData += data; }
+
+
+    req.on("end", function (err,data) 
+   { res.writeHead(200, { "content-type": "text/html" }  );
+     res.write()
+   
+   
+   
+    }
+
    // send this as part of the response
    res.write('Hello World\n');
    res.end();
-});
+}
+
+
+var myServer = http.createServer(handleRequest);
  
 var port = process.env.PORT || 80;
 myServer.listen(port); 
---
+++ Form_Example/server.js
@@ -0,0 +1,65 @@
+var http = require('http');
+ var postHTML =
+'<html><head><title>Post Example</title></head>' +
+'<body>' +
+'<form method="post">' +
+'Your Fist Name: <input name="first_name"><br>' +
+'Your Last Name: <input name="last_name"><br>' +
+'<input type="submit">' +
+'</form>' +
+'</body></html>';
+ 
+ function formValues (data)
+{
+    var splits = data.split('&');
+    var hash = [];
+    console.log(splits.length);
+    for (i = 0; i < splits.length; i++)
+    {   var iSplit = splits[i].split('=');
+        hash[iSplit[0]] = iSplit[1];
+    }
+    return hash;
+}
+
+
+function handelRequest(req,res)
+{
+    if (req.url === '/favicon.ico')
+   {
+    res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+    res.end();
+    console.log('favicon requested');
+    return;
+  }
+    var body = "";
+ 
+   req.on('data', function (chunk) 
+   { body += chunk; });
+ 
+    req.on('end', function ()
+    {  console.log('POSTed: ' + body);
+       if (body != '')
+       {  var hash = formValues(body);
+          console.log("input1 = " + hash["first_name"]);
+          console.log("input2 = " + hash["last_name"]);
+         res.writeHead(200);
+         res.write('Hello ' + hash["first_name"] + ', ' + hash["last_name"]);
+          res.end();
+          return; 
+       }
+
+     res.writeHead(200);
+     res.end(postHTML);
+    }
+   );
+
+}
+ 
+var server = http.createServer(handelRequest);
+
+
+// Port 8080 will be used if app is run from CLI
+var Port = process.env.PORT || 8080;
+
+server.listen(Port);
+ console.log("Server listening on Portt = " + Port );
